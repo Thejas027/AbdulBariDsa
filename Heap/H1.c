f0fd936f0e9fff296391f3bfdf1d1fb114fc4cd3
Thejas <teju270934@gmail.com>
2024-04-01 23:43:48 +0530

    indenteded the program

--- Heap/H1.c
+++ Heap/H1.c
@@ -5,60 +5,60 @@
 
 void Insert(int A[], int n)
 {
-  int i = n, temp;
-  temp = A[i];
+    int i = n, temp;
+    temp = A[i];
 
-  while (i > 1 && temp > A[i / 2]) // temo value is comopared to its parent value if its greater then swaping will be done
-  {
-    A[i] = A[i / 2];
-    i = i / 2; // i is moved to its parent index for further checking...
-  }
-  A[i] = temp; // value of temp is copying to index i position where ever i is pointing
+    while (i > 1 && temp > A[i / 2]) // temo value is comopared to its parent value if its greater then swaping will be done
+    {
+        A[i] = A[i / 2];
+        i = i / 2; // i is moved to its parent index for further checking...
+    }
+    A[i] = temp; // value of temp is copying to index i position where ever i is pointing
 }
 
 int delete(int A[], int n)
 {
-  int val = A[1];
-  A[1] = A[n];
-  A[n] = val;
+    int val = A[1];
+    A[1] = A[n];
+    A[n] = val;
 
-  int i = 1, j = 2 * i;
+    int i = 1, j = 2 * i;
 
-  while (j < n - 1)
-  {
-    if (j < n - 1 && A[j + 1] > A[j])
-      j = j + 1; 
-
-    if (A[i] < A[j])
+    while (j < n - 1)
     {
-      int t = A[i];
-      A[i] = A[j];
-      A[j] = t;
-      i = j;
-      j = 2 * j;
-    }
-    else
-      break;
-  }
+        if (j < n - 1 && A[j + 1] > A[j])
+            j = j + 1;
 
-  return val;
+        if (A[i] < A[j])
+        {
+            int t = A[i];
+            A[i] = A[j];
+            A[j] = t;
+            i = j;
+            j = 2 * j;
+        }
+        else
+            break;
+    }
+
+    return val;
 }
 
 int main()
 {
-  int H[] = {0, 10, 20, 30, 25, 5, 40, 35};
-  // 40,25,35,10,5,20,30   --->result should be
-  int i;
-  for (i = 2; i < 7; i++)
-    Insert(H, i);
+    int H[] = {0, 10, 20, 30, 25, 5, 40, 35};
+    // 40,25,35,10,5,20,30   --->result should be
+    int i;
+    for (i = 2; i < 7; i++)
+        Insert(H, i);
 
-  printf("Deleted Element is : %d \n ", delete (H, 7));
-  // printf("Deleted Element is : %d \n ", delete (H, 6));
+    printf("Deleted Element is : %d \n ", delete (H, 7));
+    // printf("Deleted Element is : %d \n ", delete (H, 6));
 
-  // for (i = 7; i >= 1; i--)
-  //   delete (H, i);
+    // for (i = 7; i >= 1; i--)
+    //   delete (H, i);
 
-  for (i = 1; i <= 7; i++)
-    printf("%d ", H[i]);
-  return 0;
+    for (i = 1; i <= 7; i++)
+        printf("%d ", H[i]);
+    return 0;
 }
